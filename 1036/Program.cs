8658b48f71d8010acaee8d2f7759babc44b7478a
ruanndias <ruan_ndias@hotmail.com>
2020-12-08 17:29:34 -0300

    problema resolvido 1036

---
+++ 1036/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1036
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            double a = double.Parse(valores[0]);
+            double b = double.Parse(valores[1]);
+            double c = double.Parse(valores[2]);
+
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+
+            double raiz = Math.Sqrt(delta);
+
+            if (raiz < 0 || a == 0 || delta < 0)
+                Console.WriteLine("Impossivel calcular");
+            else
+            {
+                double r1 = (-b + raiz) / (2 * a);
+                double r2 = (-b - raiz) / (2 * a);
+                Console.WriteLine("R1 = {0}", String.Format("{0:0.00000}", r1));
+                Console.WriteLine("R2 = {0}", String.Format("{0:0.00000}", r2));
+            }
+
+        }
+    }
+}
