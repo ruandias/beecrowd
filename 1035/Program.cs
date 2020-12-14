6d07455ad3476bbf60b01f66ca57923da741b371
ruanndias <ruan_ndias@hotmail.com>
2020-12-14 12:04:24 -0300

    problema resolvido 1035

---
+++ 1035/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+            int c = int.Parse(valores[2]);
+            int d = int.Parse(valores[3]);
+
+            if(b > c && d > a && ((c + d) > (a + b)) && (a % 2 == 0))
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+        }
+    }
+}
