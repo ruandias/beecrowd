d5ffa9b142af18ce65f29e583a25db6328d781a0
ruanndias <ruan_ndias@hotmail.com>
2020-12-16 12:49:43 -0300

    problema resolvido 1041

---
+++ 1041/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1041
+{
+    class Program
+    {
+        public struct Ponto
+        {
+            public float X;
+            public float Y;
+        }
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            Ponto ponto = new Ponto()
+            {
+                X = float.Parse(valores[0]),
+                Y = float.Parse(valores[1])
+            };
+
+            if (ponto.X == 0 && ponto.Y == 0)
+                Console.WriteLine("Origem");
+            else if (ponto.X > 0 && ponto.Y > 0)
+                Console.WriteLine("Q1");
+            else if (ponto.X < 0 && ponto.Y > 0)
+                Console.WriteLine("Q2");
+            else if (ponto.X < 0 && ponto.Y < 0)
+                Console.WriteLine("Q3");
+            else if (ponto.X > 0 && ponto.Y < 0)
+                Console.WriteLine("Q4");
+            else if (ponto.X == 0 && ponto.Y != 0)
+                Console.WriteLine("Eixo Y");
+            else if (ponto.X != 0 && ponto.Y == 0)
+                Console.WriteLine("Eixo X");
+        }
+    }
+}
