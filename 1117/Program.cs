b0f9a91ad5bf4ae89c61e1a97cefb388b4e3aab0
ruandias <ruan_ndias@hotmail.com>
2020-12-22 20:53:46 -0300

    problema resolvido 1117

---
+++ 1117/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = 0;
+            double media = 0, soma = 0;
+            do
+            {
+                double nota = double.Parse(Console.ReadLine());
+                if (nota < 0 || nota > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                }
+                else
+                {
+                    count++;
+                    soma += nota;
+                }
+            } while (count < 2);
+
+            media = soma / count;
+
+            Console.WriteLine("media = " + (media).ToString("N2"));
+        }
+    }
+}
