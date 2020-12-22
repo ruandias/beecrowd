e2e900614dca8ff3a02f8607638934a757ba7db0
ruandias <ruan_ndias@hotmail.com>
2020-12-22 20:35:06 -0300

    problema resolvido 1143

---
+++ 1143/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1143
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N = -1;
+            do
+            {
+                N = Int32.Parse(Console.ReadLine());
+            } while (N < 1 || N > 1000);
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"{Math.Pow(i, 1)} {Math.Pow(i, 2)} {Math.Pow(i, 3)}");
+            }
+        }
+    }
+}
