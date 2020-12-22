88a5cc32c7c7b95de230f69d0ec0e590be6b4eca
ruandias <ruan_ndias@hotmail.com>
2020-12-22 20:29:49 -0300

    problema resolvido 2936

---
+++ 2936/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2936
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Curupira = 300 * Int32.Parse(Console.ReadLine());
+            int Boitatá = 1500 * Int32.Parse(Console.ReadLine());
+            int Boto = 600 * Int32.Parse(Console.ReadLine());
+            int Mapinguari = 1000 * Int32.Parse(Console.ReadLine());
+            int Iara = 150 * Int32.Parse(Console.ReadLine());
+            int Chica = 225;
+            int total = Curupira + Boitatá + Boto + Mapinguari + Iara + Chica;
+            Console.WriteLine(total);
+        }
+    }
+}
