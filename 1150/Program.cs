fa6c86aa06fb75d5513b79b06aafb17db9662970
ruandias <ruan_ndias@hotmail.com>
2020-12-22 20:48:55 -0300

    problema resolvido 1150

---
+++ 1150/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Z, quantidade = 0, soma = 0;
+            Z = Int32.Parse(Console.ReadLine());
+
+            do
+            {
+                X = Int32.Parse(Console.ReadLine());
+            } while (X <= Z);
+
+
+            while (soma <= X)
+            {
+                soma += Z++;
+                quantidade++;
+            }
+
+            Console.WriteLine(quantidade);
+        }
+    }
+}
