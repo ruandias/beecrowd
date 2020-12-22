90bbf9c383bb500791273a8a3825678d940f91e6
ruandias <ruan_ndias@hotmail.com>
2020-12-22 20:43:57 -0300

    problema resolvido 1789

---
+++ 1789/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _1789
+{
+    class Program
+    {
+        static void Main()
+        {
+            string entrada = Console.ReadLine();
+
+            do
+            {
+                if (!string.IsNullOrEmpty(entrada))
+                {
+                    int maiorVelocidade = 0;
+                    int lesma = 0;
+                    int lesmas = Int32.Parse(entrada);
+                    string[] velocidades = Console.ReadLine().Split(' ');
+
+                    while (lesmas > 0)
+                    {
+                        int velocidadeTartaruga = Int32.Parse(velocidades[lesma]);
+
+                        if (maiorVelocidade < velocidadeTartaruga)
+                        {
+                            maiorVelocidade = velocidadeTartaruga;
+                        }
+
+                        lesmas--;
+                        lesma++;
+                    }
+
+                    int maisVeloz = maiorVelocidade;
+
+                    if (maisVeloz >= 20)
+                    {
+                        Console.WriteLine("3");
+                    }
+                    else if (maisVeloz >= 10 && maisVeloz < 20)
+                    {
+                        Console.WriteLine("2");
+                    }
+                    else
+                    {
+                        Console.WriteLine("1");
+                    }
+                }
+                entrada = Console.ReadLine();
+            } while (!string.IsNullOrEmpty(entrada));
+           
+        }
+    }
+}
