b411c576b5af034fa172cc107ec768ca86788dd2
ruandias <ruan_ndias@hotmail.com>
2020-12-22 21:47:26 -0300

    Presentation error (5%)

---
+++ 2968/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _2968
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+            int voltas = int.Parse(entrada[0]);
+
+            int placas = int.Parse(entrada[1]);
+
+            int totalPercurso = voltas * placas;
+
+            const decimal PORCENTO = 0.1M;
+            decimal passo = PORCENTO;
+
+
+            while (passo < 1)
+            {
+                decimal percorrido = totalPercurso * passo;
+                
+                Console.Write("{0} ", (int)Math.Ceiling(percorrido));
+                
+                passo += PORCENTO;
+            }
+        }
+    }
+}
