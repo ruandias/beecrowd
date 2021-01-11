e7dab14836ba1ec157f494e6cd5841a6540dc66d
ruandias <ruan_ndias@hotmail.com>
2021-01-10 21:28:20 -0300

    problema resolvido 2150

---
+++ 2150/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _2150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string vogaisAlienigenas = Console.ReadLine();
+
+                string frase = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(vogaisAlienigenas))
+                    break;
+                if (string.IsNullOrWhiteSpace(frase))
+                    break;
+                
+                int totalVogais = ContaVogal(frase, vogaisAlienigenas);
+
+                Console.WriteLine(totalVogais);
+            }
+        }
+
+        static int ContaVogal(string umaFrase, string vogaisAlienigenas)
+        {
+            int contabilizaVogais = 0;
+            for (int i = 0; i < vogaisAlienigenas.Length; i++)
+            {
+                for (int j = 0; j < umaFrase.Length; j++)
+                {
+                    if (vogaisAlienigenas[i] == umaFrase[j])
+                    {
+                        contabilizaVogais++;
+                    }
+                }
+            }
+
+            return contabilizaVogais;
+        }
+    }
+}
