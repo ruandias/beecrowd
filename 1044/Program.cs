e03f8dda2eba2264ec6721c53e2a58d718543d61
Ruan do Nascimento Dias <r.do.nascimento.dias@avanade.com>
2021-03-23 17:50:39 -0300

    problema resolvido 1044

---
+++ 1044/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int A = int.Parse(valores[0]);
+            int B = int.Parse(valores[1]);
+
+            if(A > B)
+            {
+                if(A % B == 0)
+                    Console.WriteLine("Sao Multiplos");
+                else 
+                    Console.WriteLine("Nao sao Multiplos");
+            }
+            else 
+            {
+                if(B % A == 0)
+                    Console.WriteLine("Sao Multiplos");
+                else 
+                    Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
