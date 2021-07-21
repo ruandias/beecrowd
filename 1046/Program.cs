7999a294b61fafa5743124926517f8ce2b9e6fe2
avanade.ruan <avanade.ruan@grupofleury.com.br>
2021-07-21 15:48:05 -0300

    problema resolvido 1046

---
+++ 1046/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInicial, horaFinal, duracao;
+            string[] entradaValores = Console.ReadLine().Split(' ');
+
+            horaInicial = int.Parse(entradaValores[0]);
+            horaFinal = int.Parse(entradaValores[1]);
+
+
+            //caso dia seguinte
+            if((horaInicial <= 12 && horaFinal <= horaInicial))
+            {
+                duracao = 24 - (horaInicial - horaFinal);
+            }
+            else if(horaInicial > 12 && horaFinal <= 12) 
+            {
+                duracao = (24 - horaInicial) + horaFinal;
+            }
+            else if(horaInicial == horaFinal)
+            {
+                duracao = 24;
+            }
+            else 
+            {
+                duracao = horaFinal - horaInicial;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+            
+        }
+    }
+}
