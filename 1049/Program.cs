632c6af0fac258b6569f1152865e324407463480
avanade.ruan <avanade.ruan@grupofleury.com.br>
2021-07-21 17:44:35 -0300

    problema resolvido 1049

---
+++ 1049/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _1049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+            string especie = Console.ReadLine();
+            string tipo = Console.ReadLine();
+
+            if(animal == "vertebrado")
+            {
+                if(especie == "ave")
+                {
+                    if(tipo == "carnivoro")
+                    {
+                        Console.WriteLine("aguia");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("pomba");
+                    }
+                }
+                else 
+                {
+                    if(tipo == "onivoro")
+                    {
+                        Console.WriteLine("homem");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }
+            else 
+            {
+                if(especie == "inseto")
+                {
+                    if(tipo == "hematofago")
+                    {
+                        Console.WriteLine("pulga");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("lagarta");
+                    }
+                }
+                else 
+                {
+                    if(tipo == "hematofago")
+                    {
+                        Console.WriteLine("sanguessuga");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
+        }
+    }
+}
