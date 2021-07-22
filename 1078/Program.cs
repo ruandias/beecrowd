337570c242fa95eeccfbc874f1bfae338ec3c275
avanade.ruan <avanade.ruan@grupofleury.com.br>
2021-07-22 17:48:17 -0300

    problema 1078 resolvido

---
+++ 1078/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} x {n} = {n * i}");
+            }	
+        }
+    }
+}
