4b967069e45eb0df6f04e93d963238c8d94df0be
avanade.ruan <avanade.ruan@grupofleury.com.br>
2021-07-23 11:04:40 -0300

    problema 1060 resolvido

---
+++ 1060/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1060
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidadeNumeros = 0;
+            int positivos = 0;
+
+            do 
+            {
+                double numero = double.Parse(Console.ReadLine());
+
+                if(numero != 0)
+                {
+                    if(numero > 0)
+                    {
+                        positivos++;
+                    }
+                    quantidadeNumeros++;
+                }
+            } while(quantidadeNumeros < 6);
+
+            Console.WriteLine($"{positivos} valores positivos");
+        }
+    }
+}
