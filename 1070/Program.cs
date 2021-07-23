60d720615c0ba5a81df19e9b897250bf7cb62b1c
avanade.ruan <avanade.ruan@grupofleury.com.br>
2021-07-23 10:23:26 -0300

    problema 1070 resolvido

---
+++ 1070/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int impares = 0;
+
+            while(impares < 6)
+            {
+                if(!(x % 2 == 0))
+                {
+                    Console.WriteLine(x);
+                    impares++;
+                }
+                x++;
+            }
+        }
+    }
+}
