3c002f6191585abdee639ea0791e5b95ebdd0a52
avanade.ruan <avanade.ruan@grupofleury.com.br>
2021-07-23 11:15:25 -0300

    problema 1065 resolvido

---
+++ 1064/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1064
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidadeNumeros = 0;
+            int positivos = 0;
+            double numerosPositiovs = 0;
+            double media = 0;
+
+
+            do
+            {
+                double numero = double.Parse(Console.ReadLine());
+
+                if (numero != 0)
+                {
+                    if (numero > 0)
+                    {
+                        positivos++;
+                        numerosPositiovs += numero;
+                    }
+                    quantidadeNumeros++;
+                }
+            } while (quantidadeNumeros < 6);
+
+            media = numerosPositiovs / positivos;
+
+            Console.WriteLine($"{positivos} valores positivos");
+            Console.WriteLine($"{media:N1}");
+        }
+    }
+}
