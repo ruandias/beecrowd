1ee97d991727d7752ced28b2e1f2d4f3ee3f3f46
avanade.ruan <avanade.ruan@grupofleury.com.br>
2021-07-23 10:11:33 -0300

    problema resolvido 1067

---
+++ 1067/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <=n; i++)
+            {
+                if(!(i % 2 == 0))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
