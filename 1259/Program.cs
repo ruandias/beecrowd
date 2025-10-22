4cf545119cc0469a67676b6663300d1b0bb3c2fa
Ruan Dias <ruan_ndias@hotmail.com>
2025-10-22 19:16:59 -0300

    resolved 2604 problem

---
+++ 1259/Program.cs
@@ -0,0 +1,24 @@
+﻿int quantidadeEntrada = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < quantidadeEntrada; i++)
+{
+
+    var evenList = new List<int>();
+    var oddList = new List<int>();
+
+    int numero = int.Parse(Console.ReadLine()!);
+    if (numero > 0)
+    {
+        if (numero % 2 == 0)
+        {
+            evenList.Add(numero);
+        }
+        else
+        {
+            oddList.Add(numero);
+        }
+    }
+
+
+}
+
---
+++ 2604/solution.sql
@@ -0,0 +1,3 @@
+SELECT "id", "name" FROM products
+WHERE price > 100 OR price < 10;
+
