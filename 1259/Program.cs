734093ccca5458f65040cbf382b16259c6612983
Ruan Dias <ruan.dias@vortigo.com.br>
2026-07-24 19:29:49 -0300

    problem resolved

--- 1259/Program.cs
+++ 1259/Program.cs
@@ -1,24 +1,51 @@
-﻿int quantidadeEntrada = int.Parse(Console.ReadLine()!);
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
 
-for (int i = 0; i < quantidadeEntrada; i++)
-{
+class URI {
 
-    var evenList = new List<int>();
-    var oddList = new List<int>();
-
-    int numero = int.Parse(Console.ReadLine()!);
-    if (numero > 0)
-    {
-        if (numero % 2 == 0)
+static void Main()
         {
-            evenList.Add(numero);
-        }
-        else
-        {
-            oddList.Add(numero);
-        }
-    }
+           int quantidadeEntrada = int.Parse(Console.ReadLine());
+            var evenList = new List<int>();
+            var oddList = new List<int>();
 
+           for (int i = 0; i < quantidadeEntrada; i++)
+            {   
+                int number = int.Parse(Console.ReadLine());
+
+                var even = IsEven(number);
+
+                if (even)
+                    evenList.Add(number);
+                else 
+                    oddList.Add(number);
+            }
+
+            evenList.Sort();
+            oddList.Sort();
+            oddList.Reverse();
+            
+            PrintList(evenList);
+            PrintList(oddList);
+        }
+
+        static bool IsEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static void PrintList(List<int> numbers)
+        {
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
 
 }
-
