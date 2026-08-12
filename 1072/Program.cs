c3af13d660fa22451da9358dddeed16897523035
Ruan Dias <ruan.dias@vortigo.com.br>
2026-08-12 14:20:02 -0300

    problems resolved

---
+++ 1072/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+// Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+// Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando essas informações.
+// Entrada
+
+// A primeira linha da entrada contém um valor inteiro N (N < 10000), que indica o número de casos de teste.
+// Cada caso de teste a seguir é um valor inteiro X (-107 < X <107).
+ 
+// Saída
+
+// Para cada caso, imprima quantos números estão dentro (in) e quantos valores estão fora (out) do intervalo.
+
+
+// versão menos otimizada
+// int num = int.Parse(Console.ReadLine());
+
+// var input = new List<int>();
+// var output = new List<int>();
+
+
+// for (int i = 0; i < num; i++)
+// {
+//     var n = int.Parse(Console.ReadLine());
+
+//     if (n >= 10 && n <= 20)
+//     {
+//         input.Add(n);
+//     }
+//     else
+//     {
+//         output.Add(n);
+//     }
+// }
+
+// Console.WriteLine($"{input.Count} in");
+// Console.WriteLine($"{output.Count} out");
+
+// versão otimizada
+// 1. Lê a quantidade de casos de teste
+        int n = int.Parse(Console.ReadLine());
+
+        // 2. Variáveis de contagem (Espaço O(1))
+        int inCount = 0;
+        int outCount = 0;
+
+        // 3. Processamento em tempo O(N)
+        for (int i = 0; i < n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (x >= 10 && x <= 20)
+            {
+                inCount++;
+            }
+            else
+            {
+                outCount++;
+            }
+        }
+
+        // 4. Imprime os resultados no formato esperado
+        Console.WriteLine($"{inCount} in");
+        Console.WriteLine($"{outCount} out");
---
+++ 1075/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Leia um valor inteiro N. Apresente todos os números entre 1 e 10000 que divididos por N dão resto igual a 2.
+// Entrada
+
+// A entrada contém um valor inteiro N (N < 10000).
+// Saída
+
+// Imprima todos valores que quando divididos por N dão resto = 2, um por linha.
+
+
+int n = int.Parse(Console.ReadLine());
+var max = 10000;
+
+for (int i = 1; i <= max; i ++)
+{
+    if (i % n == 2)
+        Console.WriteLine(i);
+}
+
